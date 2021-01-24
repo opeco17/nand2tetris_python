13b35d8ad447d1e6d1e64a0699ee17af512c0b09
opeco17 <opeco17@gmail.com>
2021-01-24 23:23:34 +0900

    乗算アセンブリの追加

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,25 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @i
+    M=1    // i=0
+    @R2
+    M=0    // R2=0
+(LOOP)
+    @i
+    D=M    // D=i
+    @R1
+    D=D-M  // D=i-R1
+    @END
+    D;JGT  // Go to END if (i-R1)>0
+    @R0
+    D=M    // D=R0
+    @R2
+    M=M+D  // R2=R2+R0
+    @i
+    M=M+1  // i=i+1
+    @LOOP
+    0;JMP  // LOOPへ移動
+(END)
+    @END
+    0;JMP
